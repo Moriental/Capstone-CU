3f584b34881d14dcb9af9d9b4ee3aa83536e5540
kim <youngwoo7804@gmail.com>
2025-03-17 19:59:04 +0900

    청운대 컴퓨터공학과 캡스톤 디자인 보일러 플레이트

---
+++ src/test/java/com/example/capstonedesign/CapStoneDesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.capstonedesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapStoneDesignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
